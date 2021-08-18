2f5281267c60c8d904dd8b345968ce4dc21baed8
VivaanBomma <87789807+VivaanBomma@users.noreply.github.com>
2021-08-18 18:02:25 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,42 +1,35 @@
-//declaring variables
-var box;
+var trex, trex_running, edges;
+var groundImage;
 
-//create in function setup
-function setup() {
-  createCanvas(500, 500);
-  box = createSprite(250, 250, 20, 20);
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  groundImage = loadImage("ground2.png")
 }
 
-function draw() 
-{
-  background(30);
+function setup(){
+  createCanvas(600,200);
+  trex = createSprite(50, 180, 10, 40)
+  trex.addAnimation("run", trex_running);
+trex.scale = 0.4;
 
-  if(keyDown(LEFT_ARROW)){
-  box.x-=5  
-    background("red")
-  }
 
-  if(keyDown(RIGHT_ARROW)){
-  box.x+= 5
-  background("blue")
-  }
-
-  if(keyDown(UP_ARROW)){
-    box.y-=5
-    background("pink")
-  }
-  
-  if(keyDown(DOWN_ARROW)){
-    box.y+=5
-    background("yellow")
-  }
-
-  
-
-  drawSprites()
-
+ground = createSprite(300, 180, 600, 10)
+ground.addImage(groundImage);
+ground.velocityX=-5
 }
 
+function draw(){
+  //set background color 
+  background("black");
+  
+  if(keyDown("space")){
+    trex.velocityY = -9
+  }
+  trex.velocityY = trex.velocityY+0.7;
 
-
-
+  if(ground.x<0){
+    ground.x = ground.width/2
+  }
+  trex.collide(ground)
+  drawSprites();
+}
